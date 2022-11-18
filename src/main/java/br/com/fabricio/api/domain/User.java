6d15a64761bc7bd1f7acfbd3a7bda8af0638e55a
xFabriicioJS <fabriciocostamonteiro@hotmail.com>
2022-11-18 17:17:00 -0300

    Finalização cobertura de todas as linhas

--- src/main/java/br/com/fabricio/api/domain/User.java
+++ src/main/java/br/com/fabricio/api/domain/User.java
@@ -43,27 +43,15 @@ public class User{
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public String getEmail() {
 		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	public String getPassword() {
 		return password;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
 
     
     
--- src/main/java/br/com/fabricio/api/resources/exceptions/StandardError.java
+++ src/main/java/br/com/fabricio/api/resources/exceptions/StandardError.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class StandardError {
 
@@ -16,37 +15,17 @@ public class StandardError {
 	private String error;
 	private String path;
 
-	public LocalDateTime getTimestamp() {
-		return timestamp;
-	}
 
-	public void setTimestamp(LocalDateTime timestamp) {
-		this.timestamp = timestamp;
-	}
 
 	public Integer getStatus() {
 		return status;
-	}
-
-	public void setStatus(Integer status) {
-		this.status = status;
 	}
 
 	public String getError() {
 		return error;
 	}
 
-	public void setError(String error) {
-		this.error = error;
-	}
 
-	public String getPath() {
-		return path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
-	}
 
 
 }
